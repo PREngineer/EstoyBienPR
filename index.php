769e1cc0e5800e8bcbf8e2142df0e7eed6967ffc
Jorge Pabon <pianistapr@hotmail.com>
2017-09-28 22:33:11 -0400

    1

--- index.php
+++ index.php
@@ -48,17 +48,19 @@
               <span class="icon-bar"></span>
             </button>
             <!-- Brand icon -->
-            Estoy Bien PR
+            <a class="navbar-brand" href="index.php">
+              <img src="logo.png" width="30" height="30" alt="Logo">
+            </a>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
               <li class="active">
-                <a link="buscar.php" style="cursor: pointer;">Buscar Gente</a>
+                <a link="buscar.php" style="cursor: pointer;">Buscar</a>
               </li>
               <li>
-                <a link="registrar.php" style="cursor: pointer;">Registrarme</a>
+                <a link="registrar.php" style="cursor: pointer;">Registrar</a>
               </li>
             </ul>
           </div><!-- /.navbar-collapse -->
