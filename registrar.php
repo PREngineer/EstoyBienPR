c119d93012c38446a3ad1c095d0e078a51c7409d
Jorge Pabon <pianistapr@hotmail.com>
2017-09-28 22:48:54 -0400

    a

--- registrar.php
+++ registrar.php
@@ -1,1 +1,55 @@
 <h1>Registrar persona</h1>
+
+<p>Por favor provea la siguiente información.</p>
+
+<form>
+  <div class="form-group">
+    <label for="nombres">Nombre:</label>
+    <input type="text" class="form-control" id="nombres">
+  </div>
+  <div class="form-group">
+    <label for="edad">Edad:</label>
+    <input type="text" class="form-control" id="edad">
+  </div>
+  <div class="form-group">
+    <label for="pueblo">Pueblo:</label>
+    <input type="text" class="form-control" id="pueblo">
+  </div>
+  <div class="form-group">
+    <label for="zip">Zip:</label>
+    <input type="text" class="form-control" id="zip">
+  </div>
+  <div class="form-group">
+    <label for="salud">Salud:</label>
+    <input type="text" class="form-control" id="salud">
+  </div>
+  <div class="form-group">
+    <label for="propiedad">Propiedad:</label>
+    <input type="text" class="form-control" id="propiedad">
+  </div>
+  <div class="form-group">
+    <label for="comida">Comida:</label>
+    <input type="text" class="form-control" id="comida">
+  </div>
+  <div class="form-group">
+    <label for="agua">Agua:</label>
+    <input type="text" class="form-control" id="agua">
+  </div>
+  <div class="form-group">
+    <label for="elec">Electricidad:</label>
+    <input type="text" class="form-control" id="elec">
+  </div>
+  <div class="form-group">
+    <label for="comunicacion">Comunicación:</label>
+    <input type="text" class="form-control" id="comunicacion">
+  </div>
+  <div class="form-group">
+    <label for="contacto">Contacto:</label>
+    <input type="text" class="form-control" id="contacto">
+  </div>
+  <div class="form-group">
+    <label for="otro">Otro:</label>
+    <input type="text" class="form-control" id="otro">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
